f3bc943ba999cea17c8d2d0c5f88205f2fa8d542
Vladislav Hrysiuk <vladislavgrisyuk@gmail.com>
2022-02-12 16:33:59 +0100

    replace time generating with better one

--- replacements.py
+++ replacements.py
@@ -34,3 +34,33 @@ def getReplacementsV(h: int = None, m: int = None):
         }
     return V
 
+def get_replacement_with_time(time_before, time_after):
+    
+    rSmall = randint(1, 15)
+    rSmall2 = randint(1, 4)
+    rInt = randint(255234, 999999 - rSmall)
+    rInt2 = randint(41800, 99999 - rSmall)
+    
+    d = time_before
+    d2 = time_after
+    date = str(d.day).zfill(2)+'.'+str(d.month).zfill(2)+'.'+str(d.year)
+    #time = str(d.hours).zfill(2) + ':' + str(d.minute).zfill(2)
+    rTimeHours = d.hour
+    rTimeMinutes = d.minute
+    time = str(rTimeHours).zfill(2) + ':' + str(rTimeMinutes).zfill(2)
+    
+    rTimeHours = d2.hour
+    rTimeMinutes = d2.minute
+    timePlus = str(rTimeHours).zfill(2) + ':' + str(rTimeMinutes).zfill(2)
+    V = { 
+        'Y77002287319': 'Y77002' + str(rInt),
+        'Y77002287335': 'Y77002' + str(rInt + rSmall),
+        '05.02.2022' : date,
+        '13:36' : time,
+        '13:37' : time,
+        '13:53' : timePlus,
+        '58041449' : '580' + str(rInt2),
+        '58041469' : '580' + str(rInt2+rSmall)
+        }
+    return V
+
